09855b1de1adbc177b363cb1d797607aa2dc59ee
Allan Cruz <allan.9598@gmail.com>
2022-07-28 22:00:19 -0300

    Placing pieces on the board

--- Java/Projeto_Xadrez/src/application/UI.java
+++ Java/Projeto_Xadrez/src/application/UI.java
@@ -7,7 +7,7 @@ public class UI {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++) {
-                printPiece(pieces[j][i]);
+                printPiece(pieces[i][j]);
             }
             System.out.println();
         }
