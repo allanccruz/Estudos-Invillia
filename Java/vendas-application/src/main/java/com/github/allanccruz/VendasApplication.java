70f8a06f0d633d2a515d70e594f997dc3d5291bf
Allan Cruz <allan.9598@gmail.com>
2022-09-05 10:48:32 -0300

    Implementation of Cliente controller

--- Java/vendas-application/src/main/java/com/github/allanccruz/VendasApplication.java
+++ Java/vendas-application/src/main/java/com/github/allanccruz/VendasApplication.java
@@ -17,60 +17,6 @@ import java.util.List;
 @SpringBootApplication
 public class VendasApplication {
 
-    @Bean
-    public CommandLineRunner init (@Autowired Clientes clientes, @Autowired Pedidos pedidos) {
-
-        return args -> {
-
-            System.out.println("Salvando clientes");
-            Cliente fulano = new Cliente("Fulano");
-            clientes.save(fulano);
-
-            Pedido p = new Pedido();
-            p.setCliente(fulano);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-//            Cliente cliente = clientes.findClienteFetchPedidos(fulano.getId());
-//            System.out.println(cliente);
-//            System.out.println(cliente.getPedidos());
-
-            pedidos.findByCliente(fulano).forEach(System.out::println);
-
-//            clientes.save(new Cliente("Douglas"));
-//            clientes.save(new Cliente("Pedro"));
-//
-//            List<Cliente> todosClientes = clientes.findAll();
-//            todosClientes.forEach(System.out::println);
-//
-//            System.out.println("Atualizando clientes");
-//            todosClientes.forEach(c -> {
-//                c.setNome(c.getNome() + " atualizado.");
-//                clientes.save(c);
-//            });
-//
-//            todosClientes = clientes.findAll();
-//            todosClientes.forEach(System.out::println);
-//
-//            System.out.println("Buscando clientes");
-//            clientes.findByNomeLike("Cli").forEach(System.out::println);
-//
-//            System.out.println("Deletando clientes");
-//            clientes.findAll().forEach(c -> {
-//                clientes.delete(c);
-//            });
-//
-//            todosClientes = clientes.findAll();
-//            if(todosClientes.isEmpty()){
-//                System.out.println("Nenhum cliente encontrado.");
-//            }else{
-//                todosClientes.forEach(System.out::println);
-//            }
-
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
