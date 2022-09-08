e469b6cdefa06770148dc6eef9d62208586dd483
Allan Cruz <allan.9598@gmail.com>
2022-09-08 18:21:13 -0300

    Created DTOs and lombok implementation

--- Java/vendas-application/src/main/java/com/github/allanccruz/domain/entities/Cliente.java
+++ Java/vendas-application/src/main/java/com/github/allanccruz/domain/entities/Cliente.java
@@ -1,10 +1,16 @@
 package com.github.allanccruz.domain.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Set;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 @Table (name = "cliente")
 public class Cliente {
@@ -24,8 +30,6 @@ public class Cliente {
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     private Set<Pedido> pedidos;
 
-    public Cliente() {
-    }
     public Cliente(String nome) {
         this.nome = nome;
     }
@@ -33,38 +37,6 @@ public class Cliente {
     public Cliente(Integer id, String nome) {
         this.id = id;
         this.nome = nome;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Set<Pedido> getPedidos() {
-        return pedidos;
-    }
-
-    public void setPedidos(Set<Pedido> pedidos) {
-        this.pedidos = pedidos;
     }
 
     @Override
